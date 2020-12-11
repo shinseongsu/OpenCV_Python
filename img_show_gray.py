52a099769caaa40ebf68687d8a2e57a69004d002
Shin seongsu <tlstjdtn321@naver.com>
2020-12-11 21:50:21 +0900

    이미지 열기 그레이 색깔

---
+++ img_show_gray.py
@@ -0,0 +1,11 @@
+import cv2
+
+img_file = "./img/mudo.jpg"     # 표시할 이미지 경로
+img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
+
+if img is not None:
+    cv2.imshow('IMG', img)  # 읽은 이미지를 화면에 표시
+    cv2.waitKey()  # 키가 입력될 때까지 대기
+    cv2.destroyAllWindows()  # 창 모두 닫기
+else:
+    print('No image file.')
