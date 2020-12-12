bb944e8f7ba4e95b28907f0af86636c682de27bd
Shin seongsu <tlstjdtn321@naver.com>
2020-12-12 13:46:41 +0900

    이미지 여러개 열기

---
+++ plt_imshow_subplot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import cv2
+
+img1 = cv2.imread('./img/mudo1.jpg')
+img2 = cv2.imread('./img/mudo2.jpg')
+img3 = cv2.imread('./img/mudo3.jpg')
+
+plt.subplot(1, 3, 1)
+plt.imshow(img1[:,:,(2,1,0)])
+plt.xticks([]); plt.yticks([])
+
+plt.subplot(1, 3, 2)
+plt.imshow(img2[:,:,::-1])
+plt.xticks([]); plt.yticks([])
+
+plt.subplot(1, 3, 3)
+plt.imshow(img3[:,:,::-1])
+plt.xticks([]); plt.yticks([])
+
+plt.show()
